37b4ae41f025f93c01693f01eaa38e5492205b1d
Нуржан Кеншимбаев <@local.itstep>
2018-12-18 21:33:18 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,107 @@
+/*#include<stdio.h>
+int  main()
+{
+	int n;
+	scanf("%d",&n);
+	for (int i = 1; n <= i; i++)
+	{
+		n = n * n;
+		printf("%d", n);
+	}
+}
+*/
+/*#include <stdio.h> //3 zadacha
+int main() {
+	int k;
+
+	printf("k = ");
+	scanf("%d", &k);
+	for (int i = 1; i <= k; i++)
+	{
+		printf("%d\n", k);
+	}
+
+	return 0;
+}
+*/
+/*#include <stdio.h> //4.a zadacha
+int main() {
+	int l=35;
+
+	for (int k = 20; k <= l; k++)
+	{
+		printf("%d\n", k);
+	}
+
+	return 0;
+}*/
+/*#include <stdio.h> //4.b zadacha
+int main() {
+	int l,sum;
+	scanf("%d", &l);
+
+	for (int k = 10; k <= l; k++)
+	{
+		sum = k * k;
+		printf("%d\n", sum);
+	}
+
+	return 0;
+}*/
+#include <stdio.h> //4.c zadacha
+/*int main() {
+	int l, sum;
+	scanf("%d", &l);
+
+	for (int k = 50; l <= k; l++)
+	{
+		sum = l *l*l;
+		printf("%d\n", sum);
+	}
+
+	return 0;
+}
+*/
+/*#include <stdio.h> //4.c zadacha
+int main() {
+	int a, b;
+	scanf("%d%d", &a, &b);
+
+	for (int a; a <= b; a++)
+	{
+
+		printf("%d\n", a);
+	}
+
+	return 0;
+}*/
+
+/*#include <stdio.h> //5 zadacha
+int main() {
+	int a = 20, b, sum;
+	scanf("%d", &b);
+
+	for (int i = 0; i <= b; i++)
+	{
+		sum = a * b;
+
+	}
+	printf("%d\n", sum);
+	return 0;
+}*/
+
+/*#include <stdio.h> //6 zadacha
+
+int main() {
+	int a = 453, b, sum;
+	scanf("%d", &b);
+
+	for (int i = 0; sum <= a; i++)
+	{
+		sum = b / a;
+
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+*/
